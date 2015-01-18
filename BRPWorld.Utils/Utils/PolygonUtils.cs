e88975755b0a0868ff6d6d9d3842303c222a52c4
superlloyd <ld@galador.net>
2015-01-18 17:13:08 +0930

    added some utility method: getClosestPointOnCurve

--- BRPWorld.Utils/Utils/PolygonUtils.cs
+++ BRPWorld.Utils/Utils/PolygonUtils.cs
@@ -11,6 +11,35 @@ namespace BRPWorld.Utils.Utils
 	{
 		#region distances
 
+		public static double DistanceToPoint(this Point p, Point p2)
+		{
+			return Math.Sqrt((p.X - p2.X) * (p.Y - p2.Y));
+		}
+
+		public static Point ClosestPointOnSegment(this Point p, Point start, Point end)
+		{
+			return ClosestPointOnCurve(p, SegmentCurve(start, end));
+		}
+
+		public static Point ClosestPointOnBezier(this Point p, Point start, Point cp1, Point cp2, Point end)
+		{
+			return ClosestPointOnCurve(p, CubicBezierCurve(start, cp1, cp2, end));
+		}
+
+		public static Point ClosestPointOnCurve(this Point p, Tuple<Polynomial, Polynomial> curve)
+		{
+			var dsquare = (curve.Item1 - p.X) * (curve.Item1 - p.X) + (curve.Item2 - p.Y) * (curve.Item2 - p.Y);
+			var deriv = dsquare.Derivate().Normalize();
+			var deriveRoots = deriv.FindRoots();
+			return deriveRoots
+				.Where(x => Math.Abs(x.Imaginary) < Polynomial.Epsilon && x.Real > 0 && x.Real < 1)
+				.Select(x => (double)x.Real)
+				.Concat(new double[] { 0, 1 })
+				.OrderBy(x => dsquare.Compute(x))
+				.Select(x => new Point(curve.Item1.Compute(x), curve.Item2.Compute(x)))
+				.First();
+		}
+
 		public static double DistanceToSegment(this Point p, Point start, Point end)
 		{
 			return DistanceToCurve(p, SegmentCurve(start, end));
